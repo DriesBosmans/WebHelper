07ff6a26d8f16acf2ce71ef1476f0011de4734c2
Dries Bosmans <driesbosmans@goit.be>
2022-01-08 14:11:40 +0100

    Add project files.

---
+++ WEB_voorbereiding/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WEB_voorbereiding.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
